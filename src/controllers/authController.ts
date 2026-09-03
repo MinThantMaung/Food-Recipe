1e41f048a1483c287d5422025e5f29d883004827
Min Thant Maung <minthant180@gmail.com>
2026-09-03 17:21:15 +0900

    feat: implement verifyotpPassword and resetPassword

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -763,3 +763,215 @@ export const forgetPassword = [
     });
   },
 ];
+
+
+export const verifyOtpPassword = [
+  body("type", "Invalid registration type")
+    .trim()
+    .notEmpty()
+    .isIn(["phone", "email"])
+    .withMessage("Registration type must be either phone or email"),
+  body("value").trim().notEmpty().withMessage("Value is required"),
+  body("value")
+    .if((value, { req }) => req.body.type === "phone")
+    .matches(/^[0-9]+$/)
+    .withMessage("Phone number must contain only digits")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number must be between 5 and 12 digits"),
+  body("value")
+    .if((value, { req }) => req.body.type === "email")
+    .isEmail()
+    .withMessage("Invalid email address"),
+  body("otp", "Invalid Otp")
+    .trim()
+    .notEmpty()
+    .matches(/^[0-9]+$/)
+    .isLength({ min: 6, max: 6 }),
+  body("token", "Invalid token").trim().notEmpty(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const { type, value, otp, token } = req.body;
+
+    const user = await getUserByValue(value, type);
+    checkUserIfNotExist(user);
+
+    const otprow = await getOtpByValue(value);
+    checkOtpExist(otprow);
+    let result;
+
+    const lastOtpRequest = new Date(otprow!.updatedAt).toLocaleDateString();
+    const today = new Date().toLocaleDateString();
+    const isSameDay = lastOtpRequest === today;
+    checkOtpErrorIfSameDate(isSameDay, otprow!.requestError);
+
+    if (otprow?.rememberToken !== token) {
+      const otpData = {
+        error: 5,
+      };
+      await updateOtp(otprow!.id, otpData);
+    }
+
+    const isExpired = moment().diff(otprow?.updatedAt, "minutes") > 1;
+    if (isExpired) {
+      return next(
+        createError(
+          "OTP has expired. Please request a new one.",
+          403,
+          "otpExpired",
+        ),
+      );
+    }
+    const isMatchOtp = await bcrypt.compare(otp, otprow?.otpCode || "");
+
+    if (!isMatchOtp) {
+      if (!isSameDay) {
+        const otpData = {
+          requestError: 1,
+        };
+        await updateOtp(otprow!.id, otpData);
+      } else {
+        const otpData = {
+          requestError: {
+            increment: 1,
+          },
+        };
+        await updateOtp(otprow!.id, otpData);
+      }
+      return next(createError("OTP is not correct!", 401, "invalidOtp"));
+    }
+
+    const verifyToken = generateToken();
+    const otpData = {
+      verifyToken,
+      requestError: 0,
+      attemptCount: 1,
+      verifyAt: new Date(),
+    };
+
+    result = await updateOtp(otprow!.id, otpData);
+    res.status(200).json({
+      message: "You are Successfully Verified for reset password!",
+      value: result.recipient,
+      token: result.verifyToken,
+    });
+  },
+];
+
+export const resetPassword = [
+  body("type", "Invalid registration type")
+    .trim()
+    .notEmpty()
+    .isIn(["phone", "email"])
+    .withMessage("Registration type must be either phone or email"),
+  body("value").trim().notEmpty().withMessage("Value is required"),
+  body("value")
+    .if((value, { req }) => req.body.type === "phone")
+    .matches(/^[0-9]+$/)
+    .withMessage("Phone number must contain only digits")
+    .isLength({ min: 5, max: 12 })
+    .withMessage("Phone number must be between 5 and 12 digits"),
+  body("value")
+    .if((value, { req }) => req.body.type === "email")
+    .isEmail()
+    .withMessage("Invalid email address"),
+    body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8, max: 72 })
+    .withMessage("Password must be between 8 and 72 characters")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[^A-Za-z0-9]/)
+    .withMessage("Password must contain at least one special character"),
+  body("token", "Invalid token").trim().notEmpty(),
+  async (req: Request, res: Response,next: NextFunction) => {
+    const errors = validationResult(req).array({ onlyFirstError: true });
+    if (errors.length > 0) {
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
+    }
+
+    const { type, value, token, password } = req.body;
+
+    const user = await getUserByValue(value, type);
+    checkUserIfNotExist(user);
+
+    const otpRow = await getOtpByValue(value);
+    checkOtpExist(otpRow);
+
+    if (otpRow?.verifyToken != token) {
+      return next(
+        createError("You are not authenticated user!", 400, errorCode.attack),
+      );
+    }
+
+    const isExpired = moment().diff(otpRow?.updatedAt, "minutes") > 1;
+    if (isExpired) {
+      return next(
+        createError(
+          "OTP has expired. Please request a new one.",
+          403,
+          "otpExpired",
+        ),
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const accessTokenPayload = {
+      id: user!.id,
+    };
+
+    const refreshTokenPayload = {
+      id: user!.id,
+      recipient:
+        user!.email === null || undefined ? user!.phone : user!.email,
+    };
+
+    const accessToken = jwt.sign(
+      accessTokenPayload,
+      process.env.ACCESS_TOKEN_SECRET!,
+      { expiresIn: 60 * 15 },
+    ); // 15min
+
+    const refreshToken = jwt.sign(
+      refreshTokenPayload,
+      process.env.REFRESH_TOKEN_SECRET!,
+      { expiresIn: 60 * 60 * 30 * 24 },
+    ); //1 month
+
+    const userData = {
+      password : hashPassword,
+      refreshToken
+    }
+
+    await updateUser(user!.id,userData);
+
+    res
+      .cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 15 * 60 * 1000,
+      })
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30days
+      })
+      .status(201)
+      .json({
+        message: "Successfully created new account",
+        userid: user!.id,
+      });
+  }
+]
